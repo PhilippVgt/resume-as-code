d3b1dbfbbec08fcc91471954052929201d7b5e9b
Philipp Voigt <philipp.voigt@daimler.com>
2022-04-17 11:23:52 +0200

    Implemented template processor based on html/template

---
+++ app/service/template_processor.go
@@ -0,0 +1,75 @@
+package service
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PhilippVgt/resume-as-code/app/model"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"html/template"
+)
+
+const (
+	templateIndexFile      = "index.html"
+	templateResourceFolder = "res"
+
+	BasicTemplatePath = "templates/basic"
+)
+
+func FillTemplate(templatePath string, resume *model.Resume) (*http.ServeMux, error) {
+	log.Infof("using template from: %s", templatePath)
+
+	htmlTemplate, err := template.ParseFiles(path.Join(templatePath, templateIndexFile))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse template %s: %v", templatePath, err)
+	}
+
+	localPhoto := false
+	localPhotoDir := ""
+	// If the user photo is a local file, we serve its directory and reference the local server instead
+	// If the photo is a URL, we leave it unchanged (chromedp will resolve it)
+	if isUrl(resume.Personal.Photo) {
+		log.Infof("using photo from URL: %s", resume.Personal.Photo)
+	} else {
+		log.Infof("using photo from local file: %s", resume.Personal.Photo)
+		localPhotoDir = filepath.Dir(resume.Personal.Photo)
+		resume.Personal.Photo = "/photo/" + filepath.Base(resume.Personal.Photo)
+		localPhoto = true
+	}
+
+	buf := new(bytes.Buffer)
+	err = htmlTemplate.Execute(buf, resume)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fill template with resume: %v", err)
+	}
+	log.Infof("successfully prepared template: %s", templatePath)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/html")
+		io.WriteString(res, strings.TrimSpace(buf.String()))
+		log.Debug("local test server served template html")
+	})
+
+	templateFiles := http.FileServer(http.Dir(path.Join(templatePath, templateResourceFolder)))
+	mux.Handle("/res/", http.StripPrefix("/res", templateFiles))
+
+	if localPhoto {
+		photoFiles := http.FileServer(http.Dir(path.Join(localPhotoDir)))
+		mux.Handle("/photo/", http.StripPrefix("/photo", photoFiles))
+	}
+
+	return mux, nil
+}
+
+func isUrl(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
